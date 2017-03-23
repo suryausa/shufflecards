5e8f2dd219fa4a05807cb783e010ee907dc3b587
Surya <Surya>
2017-03-23 14:14:55 -0400

    commited code  removed oops

---
+++ OOPS/Shuffle/src/cards/constants/CommonConstants.java
@@ -0,0 +1,13 @@
+package cards.constants;
+
+
+
+public class CommonConstants {
+	
+	public static final String  JOKER="JOKER";
+	
+	public static final String[] SUITS = { "Spades", "Hearts", "Diamonds", "Clubs" };
+	public static final String[] RANKS = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+
+
+}
